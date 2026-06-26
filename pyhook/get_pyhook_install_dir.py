a9563287223bb410660000ba25e6470bcca93afa
Nad Alaba <37968805+nadalaba@users.noreply.github.com>
2026-06-26 22:56:53 +0300

    fix: remove extra leading 'local/' when python < 3.10 too
    sysconfig._get_default_scheme() exists on python < 3.10

--- pyhook/get_pyhook_install_dir.py
+++ pyhook/get_pyhook_install_dir.py
@@ -4,20 +4,21 @@ import sysconfig as sc
 install_dir_full = sys.argv[1]
 is_apple = len(sys.argv) > 2 and sys.argv[2] == "APPLE"
 
-if is_apple:
-    p = sc.get_path("platlib", "posix_prefix", vars={"platbase": "."})
-else:
-    p = sc.get_path("platlib", vars={"platbase": "."})
+if is_apple: scheme = "posix_prefix"
+# sc.get_default_scheme() exists only in Python >= 3.10.
+# Previously, it was called _get_default_scheme().
+elif hasattr(sc, "get_default_scheme"): scheme = sc.get_default_scheme()
+else: scheme = sc._get_default_scheme()
 
-    # sc.get_default_scheme() does not exist in Python < 3.10
-    scheme = getattr(sc, "get_default_scheme", lambda: None)()
-    # "posix_local" prepends "local/", causing double "local/" if installing to
-    # /usr/local. Remove it unless installing directly to platbase (e.g., /usr).
-    if (
-        scheme == "posix_local" and
-        install_dir_full != sc.get_config_var("platbase") and
-        p.startswith("local/")
-    ):
-        p = p[6:]
+p = sc.get_path("platlib", scheme, vars={"platbase": "."})
+
+# "posix_local" prepends "local/", causing double "local/" if installing to
+# /usr/local. Remove it unless installing directly to platbase (e.g., /usr).
+if (
+    scheme == "posix_local" and
+    install_dir_full != sc.get_config_var("platbase") and
+    p.startswith("local/")
+):
+    p = p[6:]
 
 print(p)
